7bd00effc1174639df70bd6b008a6a1b3b9d0b8b
Neelraj <subhash.nitta@gmail.com>
2025-03-27 15:27:12 -0400

    connection to db established

--- facial_recog_encrypted/facial_recog_encrypted/settings.py
+++ facial_recog_encrypted/facial_recog_encrypted/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'facial_recog_encrypted.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'facial_recognition_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Subha@2024',
+        'HOST': 'localhost',  # or your host
+        'PORT': '5432',       # default PostgreSQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
