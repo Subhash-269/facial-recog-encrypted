b93335656e0964d882d21ee3bdc3c675e648979c
Neelraj <subhash.nitta@gmail.com>
2025-03-27 10:15:24 -0400

    updated with encode;decode apis

--- facial_recog_encrypted/backend/urls.py
+++ facial_recog_encrypted/backend/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import hello_api
+from .views import hello_api,encrypt_images_api,decrypt_images_api
 
 urlpatterns = [
-    path('hello/', hello_api),
+    path('hello/', hello_api),  
+    path('encrypt/', encrypt_images_api, name='encrypt_image_api'),
+    path('decrypt/', decrypt_images_api, name='decrypt_images_api'),
 ]
