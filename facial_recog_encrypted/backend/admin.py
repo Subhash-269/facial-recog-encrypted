fb6e17ea0bbf55b520e1b83a0940e87d82b8907e
Neelraj <subhash.nitta@gmail.com>
2025-03-27 15:26:26 -0400

    updated admin with new tables

--- facial_recog_encrypted/backend/admin.py
+++ facial_recog_encrypted/backend/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Image, EncryptedChunk
+
+class EncryptedChunkInline(admin.TabularInline):
+    model = EncryptedChunk
+    extra = 0
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('filename', 'created_at')
+    inlines = [EncryptedChunkInline]
+
+@admin.register(EncryptedChunk)
+class EncryptedChunkAdmin(admin.ModelAdmin):
+    list_display = ('image', 'chunk_index')
